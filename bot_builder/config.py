55361139ba4cdf888822a63307e603c1b386debb
TuanMudaYonzz <143253401+notyon@users.noreply.github.com>
2025-06-28 10:39:01 +0700

    Update config.py

--- bot_builder/config.py
+++ bot_builder/config.py
@@ -1,11 +1,22 @@
+
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Ambil dari file .env jika tersedia
+# Load variabel dari .env file
+load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
-MONGO_URI = os.getenv("MONGO_URI", "")
-FORCE_JOIN_CHANNEL = os.getenv("FORCE_JOIN_CHANNEL", "")
-FORCE_JOIN_NAME = os.getenv("FORCE_JOIN_NAME", "")  # Misalnya: 'leobaseid'
+# Telegram Bot Token dari @BotFather
+BOT_TOKEN = os.getenv("8068737897:AAEC4EfVnn-GeN7Yo0oMRpxcY0_eYCjHVLo")
+
+# ID Admin bot pusat (int)
+ADMIN_ID = int(os.getenv("1684865940"))
+
+# MongoDB URI
+MONGO_URI = os.getenv("mongodb+srv://ucik:ucik@cluster0.0l3r8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+# API_ID & API_HASH wajib untuk autentikasi Pyrogram
+API_ID = int(os.getenv("29486311"))
+API_HASH = os.getenv("ffdc688dc4eee8d2585cb24155188432")
+FORCE_JOIN_CHANNEL = os.getenv("FORCE_JOIN_CHANNEL", "1001991260453")
+FORCE_JOIN_NAME = os.getenv("FORCE_JOIN_NAME", "leobaseid")  # Misalnya: 'leobaseid'
 GCAST_BLACKLIST = os.getenv("GCAST_BLACKLIST", "").split("|")  # pisahkan pakai '|'# Configuration for bot_builder
