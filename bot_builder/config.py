e2d95d1df5877267271e86e0d69228ac09152bc6
TuanMudaYonzz <143253401+notyon@users.noreply.github.com>
2025-06-28 09:55:09 +0700

    Update config.py

--- bot_builder/config.py
+++ bot_builder/config.py
@@ -1,1 +1,11 @@
-# Configuration for bot_builder
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Ambil dari file .env jika tersedia
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+MONGO_URI = os.getenv("MONGO_URI", "")
+FORCE_JOIN_CHANNEL = os.getenv("FORCE_JOIN_CHANNEL", "")
+FORCE_JOIN_NAME = os.getenv("FORCE_JOIN_NAME", "")  # Misalnya: 'leobaseid'
+GCAST_BLACKLIST = os.getenv("GCAST_BLACKLIST", "").split("|")  # pisahkan pakai '|'# Configuration for bot_builder
