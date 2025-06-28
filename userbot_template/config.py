af1022ec16013e634efef74e62ec3e55094b449b
TuanMudaYonzz <143253401+notyon@users.noreply.github.com>
2025-06-28 10:00:46 +0700

    Update config.py

--- userbot_template/config.py
+++ userbot_template/config.py
@@ -1,1 +1,11 @@
-# Config file for userbot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", 123456))
+API_HASH = os.getenv("API_HASH", "your_api_hash_here")
+SESSION = os.getenv("SESSION", "")
+FORCE_JOIN_CHANNEL = os.getenv("FORCE_JOIN_CHANNEL", "")
+GCAST_BLACKLIST = os.getenv("GCAST_BLACKLIST", "").split("|")
+SUDO_USERS = os.getenv("SUDO_USERS", "").split("|")  # user_id list# Config file for userbot
